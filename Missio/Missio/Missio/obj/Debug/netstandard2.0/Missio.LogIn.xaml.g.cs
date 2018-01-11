13137340aa06e8557f9067d2902d1a4c06dc578f
Alejandro Romero <a310938@uach.mx>
2018-01-10 22:04:49 -0700

    change

--- Missio/Missio/Missio/obj/Debug/netstandard2.0/Missio.LogIn.xaml.g.cs
+++ Missio/Missio/Missio/obj/Debug/netstandard2.0/Missio.LogIn.xaml.g.cs
@@ -11,12 +11,20 @@
 namespace Missio {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("D:\\Code\\Missio\\Missio\\Missio\\Missio\\LogIn.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Jorge\\source\\repos\\Missio\\Missio\\Missio\\Missio\\LogIn.xaml")]
     public partial class LogIn : global::Xamarin.Forms.ContentPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Entry User;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Entry Password;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(LogIn));
+            User = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "User");
+            Password = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Password");
         }
     }
 }
--- Missio/Missio/Missio/obj/Debug/netstandard2.0/Missio.MainPage.xaml.g.cs
+++ Missio/Missio/Missio/obj/Debug/netstandard2.0/Missio.MainPage.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace Missio {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("c:\\users\\jorge\\source\\repos\\Missio\\Missio\\Missio\\MainPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Jorge\\source\\repos\\Missio\\Missio\\Missio\\Missio\\MainPage.xaml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
