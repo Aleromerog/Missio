fcc597b712e740b961cfca1f4ec487e7fb5cce29
Francisco Greco <francisco@greco.com.mx>
2018-02-05 20:30:27 -0700

    Format Android Main Activity

--- Missio/Missio/Missio.Android/MainActivity.cs
+++ Missio/Missio/Missio.Android/MainActivity.cs
@@ -5,16 +5,15 @@ using Android.OS;
 
 namespace Missio.Droid
 {
-    [Activity(Label = "Missio", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Missio", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(bundle);
-
             Xamarin.Forms.Forms.Init(this, bundle);
             try
             {
@@ -24,8 +23,6 @@ namespace Missio.Droid
             {
                 throw exception.InnerException;
             }
-            
         }
     }
 }
-
