7f4775af158b1d1e2334787f6d0508e7042bc267
Alejandro Romero <a310938@uach.mx>
2018-02-15 17:01:28 -0700

    Botones prueba

---
+++ Missio/Missio/Missio.Android/CustomRenders/ButtonLoginRender.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Missio.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(ButtonLoginRender))]
+
+namespace Missio.Droid
+{
+    public class ButtonLoginRender : Xamarin.Forms.Platform.Android.AppCompat.ButtonRenderer
+    {
+        public ButtonLoginRender(Context context): base(context)
+        {
+            
+        } 
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            var boton = new GradientDrawable();
+            boton.SetCornerRadius(40f);
+            boton.SetColor(Android.Graphics.Color.Snow);
+        }
+    }
+
+}
