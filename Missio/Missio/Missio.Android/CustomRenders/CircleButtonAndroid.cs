b45823d52eb9e7b32beceea57d11ea8d2ce1a2ab
Alejandro Romero <a310938@uach.mx>
2018-02-18 19:37:43 -0700

    Login, bugs and buttons

---
+++ Missio/Missio/Missio.Android/CustomRenders/CircleButtonAndroid.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Missio.Droid.CustomRenders;
+using Missio.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Button = Xamarin.Forms.Button;
+
+[assembly: ExportRenderer(typeof(CircleButton), typeof(CircleButtonAndroid))]
+
+namespace Missio.Droid.CustomRenders
+{
+    class CircleButtonAndroid : ButtonRenderer
+    {
+        public CircleButtonAndroid(Context context) : base(context)
+        {
+        }
+        
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+
+            }
+
+            if (e.OldElement != null)
+            {
+
+            }
+            if(Control != null)
+            {
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetShape(ShapeType.Rectangle);
+                gradientDrawable.SetCornerRadius(100);
+                gradientDrawable.SetColor(Element.BackgroundColor.ToAndroid());
+                Control.SetBackground(gradientDrawable);
+            }
+        }
+    }
+}
