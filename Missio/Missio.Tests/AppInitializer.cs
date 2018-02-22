930bcef8b5be0b2851560c452ae11e7005fd8292
Francisco Greco <francisco@greco.com.mx>
2018-02-21 23:15:14 -0700

    Hacer pruebas automatizadas del LogIn

---
+++ Missio/Missio.Tests/AppInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace Missio.Tests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android.InstalledApp("com.ChomeDev.Missio")
+                    .StartApp();
+            }
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT) // Cant run iOS test on windows
+                Assert.Pass();
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
---
+++ Missio/Missio.Tests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace Missio.Tests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        private IApp app;
+        private readonly Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        [TestCase("Non existing user 1")]
+        [TestCase("Non existing user 2")]
+        public void LogIn_InvalidUserName_DisplaysNonAvailableUserAlert(string invalidUserName)
+        {
+            // Arrange
+            app.EnterText(c => c.Marked("UserNameEntry"), invalidUserName);
+            // Act
+            app.Tap(c => c.Marked("LogInCommand"));
+            // Assert
+            app.WaitForElement(c => c.Text("There does not exist a user with the given name"));
+        }
+    }
+}
+
