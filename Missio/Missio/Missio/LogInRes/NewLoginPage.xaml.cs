f3e590014cff6b92c879b37ce33973d93ff3a02b
AlejandroRomero <a310938@uach.mx>
2018-07-05 10:37:19 -0600

    LoginPage Listo

--- Missio/Missio/Missio/LogInRes/NewLoginPage.xaml.cs
+++ Missio/Missio/Missio/LogInRes/NewLoginPage.xaml.cs
@@ -3,17 +3,17 @@ using Xamarin.Forms;
 
 namespace Missio.LogInRes
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewLoginPage
-	{
-		public NewLoginPage ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewLoginPage
+    {
+        public NewLoginPage ()
+        {
+            InitializeComponent ();
             background.Source = ImageSource.FromResource("Missio.Images.LogInBackground2.png");
             square.Source = ImageSource.FromResource("Missio.Images.square.png");
             lenguas.Source = ImageSource.FromResource("Missio.Images.logo.png");
+            
 
-
-		}
-	}
+        }
+    }
 }
---
+++ Missio/Missio/Missio/LogInRes/NewRegistraionPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Missio.LogInRes
+{
+    public partial class NewRegistraionPage : ContentPage
+    {
+        public NewRegistraionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
