5a0f7cc4326e78b888ce6c0a17522cef2557ef90
Francisco Greco <francisco@greco.com.mx>
2018-11-12 18:06:59 -0700

    News feed page dummy posts

--- Missio/Missio.NewsFeed/NewsFeedPage.xaml.cs
+++ Missio/Missio.NewsFeed/NewsFeedPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net;
 using JetBrains.Annotations;
 using Missio.Posts;
+using Missio.Users;
 using Xamarin.Forms.Xaml;
 
 namespace Missio.NewsFeed
@@ -27,9 +29,18 @@ namespace Missio.NewsFeed
         public class ListModel{
             static System.DateTime time = System.DateTime.Now;
 
-            public List<IPost> Posts { get; set; } = new List<IPost>{
-                new Post(new Users.User("Jorge", null, "Som@mail.com"), "El mensaje inspirador", time, null)
-            };
+            public List<IPost> Posts { get; set; } = new List<IPost>();
+
+            public ListModel()
+            {
+                byte[] userImage;
+                using (var webClient = new WebClient())
+                {
+                    userImage = webClient.DownloadData("TuURL");
+                }
+
+                Posts.Add(new Post(new User("Jorge", userImage, "Som@mail.com"), "El mensaje inspirador", time, null));
+            }
         }
 	}
 }
