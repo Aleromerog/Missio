b629d8a7a0e05b6108dc4878ec2edd7a0136a070
Francisco Greco <francisco@greco.com.mx>
2018-12-19 15:57:59 -0700

    Arreglar comments view model que no implementaba INotifyPropertyChanged

--- Missio/ViewModels/CommentsViewModel.cs
+++ Missio/ViewModels/CommentsViewModel.cs
@@ -8,9 +8,15 @@ namespace ViewModels
         Post Post { get; set; }
     }
 
-    public class CommentsViewModel : ICommentsViewModel
+    public class CommentsViewModel : ViewModel, ICommentsViewModel
     {
+        private Post _post;
+
         [UsedImplicitly]
-        public Post Post { get; set; }
+        public Post Post
+        {
+            get => _post;
+            set => SetField(ref _post, value);
+        }
     }
 }
