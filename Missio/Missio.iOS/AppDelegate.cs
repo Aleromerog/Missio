7e15869a3601c7b604ea190a3cf27e31ad4107a4
Alejandro Romero <a310938@uach.mx>
2019-01-03 23:50:40 -0700

    New Login Ready

--- Missio/Missio.iOS/AppDelegate.cs
+++ Missio/Missio.iOS/AppDelegate.cs
@@ -7,6 +7,9 @@ using UXDivers.Gorilla.iOS;
 #endif
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using Missio.CustomRenderers;
+
+
 
 namespace Missio.iOS
 {
@@ -16,13 +19,14 @@ namespace Missio.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             Forms.Init();
-            #if GORILLA
-            LoadApplication(Player.CreateApplication(new UXDivers.Gorilla.Config("Gorilla on Jorges").RegisterAssembliesFromTypes<ByteArrayToImageSourceConverter, NewsFeedPostDataTemplateSelector>()));
-            #else
+#if GORILLA
+            LoadApplication(Player.CreateApplication(new UXDivers.Gorilla.Config("Gorilla on Jorges").RegisterAssembliesFromTypes<ByteArrayToImageSourceConverter, DevsDNA.GradientBoxView, CustomEntry,NewsFeedPostDataTemplateSelector>()));
+#else
             LoadApplication(new App("https://localhost:44333/"));
             #endif
             UITabBar.Appearance.SelectedImageTintColor = UIColor.FromRGB(88, 3, 1);
             UINavigationBar.Appearance.BarTintColor = UIColor.FromRGB(88, 3, 1);
+
             //UITabBar.Appearance.BarTintColor = UIColor.FromRGB(88, 3, 1);
             return base.FinishedLaunching(app, options);
         }
---
+++ Missio/Missio.iOS/Custom Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Missio.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace Missio.iOS.CustomRenderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.Layer.CornerRadius = 10;
+                Control.TextColor = UIColor.White;
+
+            }
+        }
+    }
+}
